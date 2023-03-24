fda1f2c1075c0a606affa8738796b5b75c9a6baf
shaff28 <shafookurdi@gmail.com>
2023-03-24 18:06:15 +0300

    AddmynetworkScreen

---
+++ lib/shafiqa/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:linkedin_clone_project/shafiqa/mynetwork.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+       debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: const MyNetwork()
+    );
+  }
+}
+
